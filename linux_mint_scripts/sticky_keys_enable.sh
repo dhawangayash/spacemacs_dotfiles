3be3bae4b22fb3b4f49bca5658586177d26584be
dgg <dgayash@gmail.com>
2022-07-30 13:19:02 -0700

    minor modifications to symbol link table

--- linux_mint_scripts/sticky_keys_enable.sh
+++ linux_mint_scripts/sticky_keys_enable.sh
@@ -2,6 +2,8 @@
 # This script sets sticky keys
 # Shortcut for this is <C-M-S s>
 
+#URL: https://wiki.ubuntu.com/Accessibility/doc/Guide/Mobility#example
+#URL: https://askubuntu.com/questions/120010/how-do-i-turn-on-sticky-keys-from-the-cli
 xkbset exp -bell -sticky -twokey -latchlock -accessx -feedback -stickybeep -led 9999
 
 # If you would prefer pressing Shift twice to mean "hold down shift" then change '-latchlock' above to 'latchlock'
