9517734c55ada9a856153e5da6251c5a80062c92
dgg <dgayash@gmail.com>
2022-10-19 12:50:13 -0700

    Got some functionality going

--- dgg-xah-functions.el
+++ dgg-xah-functions.el
@@ -360,5 +360,118 @@ Version 2017-05-30"
         (re-search-forward "\n[\t\n ]*\n+" nil "NOERROR" ))
     (end-of-line)))
 
-(global-set-key (kbd "<left>") 'xah-beginning-of-line-or-block)
-(global-set-key (kbd "<right>") 'xah-end-of-line-or-block)
+;; (global-set-key (kbd "<left>") 'xah-beginning-of-line-or-block)
+;; (global-set-key (kbd "<right>") 'xah-end-of-line-or-block)
+(define-key evil-motion-state-map (kbd "<left>") 'xah-beginning-of-line-or-block)
+(define-key evil-motion-state-map (kbd "<right>") 'xah-end-of-line-or-block)
+
+(defun xah-open-file-at-cursor ()
+  "Open the file path under cursor.
+If there is text selection, uses the text selection for path.
+If the path starts with “http://”, open the URL in browser.
+Input path can be {relative, full path, URL}.
+Path may have a trailing “:‹n›” that indicates line number, or “:‹n›:‹m›” with line and column number. If so, jump to that line number.
+If path does not have a file extension, automatically try with “.el” for elisp files.
+This command is similar to `find-file-at-point' but without prompting for confirmation.
+
+URL `http://xahlee.info/emacs/emacs/emacs_open_file_path_fast.html'
+Version 2020-10-17"
+  (interactive)
+  (let* (
+         ($inputStr
+          (if (use-region-p)
+              (buffer-substring-no-properties (region-beginning) (region-end))
+            (let ($p0 $p1 $p2
+                      ;; chars that are likely to be delimiters of file path or url, e.g. whitespace, comma. The colon is a problem. cuz it's in url, but not in file name. Don't want to use just space as delimiter because path or url are often in brackets or quotes as in markdown or html
+                      ($pathStops "^  \t\n\"`'‘’“”|[]{}「」<>〔〕〈〉《》【】〖〗«»‹›❮❯❬❭〘〙·。\\"))
+              (setq $p0 (point))
+              (skip-chars-backward $pathStops)
+              (setq $p1 (point))
+              (goto-char $p0)
+              (skip-chars-forward $pathStops)
+              (setq $p2 (point))
+              (goto-char $p0)
+              (buffer-substring-no-properties $p1 $p2))))
+         ($path
+          (replace-regexp-in-string
+           "^file:///" "/"
+           (replace-regexp-in-string
+            ":\\'" "" $inputStr))))
+    (if (string-match-p "\\`https?://" $path)
+        (if (fboundp 'xahsite-url-to-filepath)
+            (let (($x (xahsite-url-to-filepath $path)))
+              (if (string-match "^http" $x )
+                  (browse-url $x)
+                (find-file $x)))
+          (progn (browse-url $path)))
+      (progn ; not starting “http://”
+        (if (string-match "#" $path )
+            (let (
+                  ( $fpath (substring $path 0 (match-beginning 0)))
+                  ( $fractPart (substring $path (1+ (match-beginning 0)))))
+              (if (file-exists-p $fpath)
+                  (progn
+                    (find-file $fpath)
+                    (goto-char (point-min))
+                    (search-forward $fractPart ))
+                (when (y-or-n-p (format "file no exist: 「%s」. Create?" $fpath))
+                  (find-file $fpath))))
+          (if (string-match "^\\`\\(.+?\\):\\([0-9]+\\)\\(:[0-9]+\\)?\\'" $path)
+              (let (
+                    ($fpath (match-string 1 $path))
+                    ($line-num (string-to-number (match-string 2 $path))))
+                (if (file-exists-p $fpath)
+                    (progn
+                      (find-file $fpath)
+                      (goto-char (point-min))
+                      (forward-line (1- $line-num)))
+                  (when (y-or-n-p (format "file no exist: 「%s」. Create?" $fpath))
+                    (find-file $fpath))))
+            (if (file-exists-p $path)
+                (progn ; open f.ts instead of f.js
+                  (let (($ext (file-name-extension $path))
+                        ($fnamecore (file-name-sans-extension $path)))
+                    (if (and (string-equal $ext "js")
+                             (file-exists-p (concat $fnamecore ".ts")))
+                        (find-file (concat $fnamecore ".ts"))
+                      (find-file $path))))
+              (if (file-exists-p (concat $path ".el"))
+                  (find-file (concat $path ".el"))
+                (when (y-or-n-p (format "file no exist: 「%s」. Create?" $path))
+                  (find-file $path ))))))))))
+
+(global-set-key (kbd "<C-return>") 'xah-open-file-at-cursor)
+
+;; Emacs: Jump to Matching Bracket
+;; http://xahlee.info/emacs/emacs/emacs_goto_matching_brackets.html
+(defvar xah-brackets '("“”" "()" "[]" "{}" "<>" "＜＞" "（）" "［］" "｛｝" "⦅⦆" "〚〛" "⦃⦄" "‹›" "«»" "「」" "〈〉" "《》" "【】" "〔〕" "⦗⦘" "『』" "〖〗" "〘〙" "｢｣" "⟦⟧" "⟨⟩" "⟪⟫" "⟮⟯" "⟬⟭" "⌈⌉" "⌊⌋" "⦇⦈" "⦉⦊" "❛❜" "❝❞" "❨❩" "❪❫" "❴❵" "❬❭" "❮❯" "❰❱" "❲❳" "〈〉" "⦑⦒" "⧼⧽" "﹙﹚" "﹛﹜" "﹝﹞" "⁽⁾" "₍₎" "⦋⦌" "⦍⦎" "⦏⦐" "⁅⁆" "⸢⸣" "⸤⸥" "⟅⟆" "⦓⦔" "⦕⦖" "⸦⸧" "⸨⸩" "｟｠")
+  "A list of strings, each element is a string of 2 chars, the left bracket and a matching right bracket. Used by `xah-select-text-in-quote' and others.")
+
+(defconst xah-left-brackets
+  '("“" "(" "[" "{" "<" "＜" "（" "［" "｛" "⦅" "〚" "⦃" "‹" "«" "「" "〈" "《" "【" "〔" "⦗" "『" "〖" "〘" "｢" "⟦" "⟨" "⟪" "⟮" "⟬" "⌈" "⌊" "⦇" "⦉" "❛" "❝" "❨" "❪" "❴" "❬" "❮" "❰" "❲" "〈" "⦑" "⧼" "﹙" "﹛" "﹝" "⁽" "₍" "⦋" "⦍" "⦏" "⁅" "⸢" "⸤" "⟅" "⦓" "⦕" "⸦" "⸨" "｟")
+  "List of left bracket chars. Each element is a string.")
+
+(defconst xah-right-brackets
+  '("”" ")" "]" "}" ">" "＞" "）" "］" "｝" "⦆" "〛" "⦄" "›" "»" "」" "〉" "》" "】" "〕" "⦘" "』" "〗" "〙" "｣" "⟧" "⟩" "⟫" "⟯" "⟭" "⌉" "⌋" "⦈" "⦊" "❜" "❞" "❩" "❫" "❵" "❭" "❯" "❱" "❳" "〉" "⦒" "⧽" "﹚" "﹜" "﹞" "⁾" "₎" "⦌" "⦎" "⦐" "⁆" "⸣" "⸥" "⟆" "⦔" "⦖" "⸧" "⸩" "｠")
+  "List of right bracket chars. Each element is a string.")
+
+(defun xah-goto-matching-bracket ()
+  "Move cursor to the matching bracket.
+If cursor is not on a bracket, call `backward-up-list'.
+The list of brackets to jump to is defined by `xah-left-brackets' and `xah-right-brackets'.
+
+URL `http://xahlee.info/emacs/emacs/emacs_navigating_keys_for_brackets.html'
+Version: 2016-11-22"
+  (interactive)
+  (if (nth 3 (syntax-ppss))
+      (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)
+    (cond
+     ((eq (char-after) ?\") (forward-sexp))
+     ((eq (char-before) ?\") (backward-sexp ))
+     ((looking-at (regexp-opt xah-left-brackets))
+      (forward-sexp))
+     ((looking-back (regexp-opt xah-right-brackets) (max (- (point) 1) 1))
+      (backward-sexp))
+     (t (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)))))
+
+(global-set-key (kbd "\\") 'xah-goto-matching-bracket)
